be65a75620ec53d357c09918496b4f91ee5bd56b
Mangesh <mdpatankar@gmail.com>
2020-10-14 16:42:34 +0530

    Updates

---
+++ precache-manifest.65f896dbdc07b8a19fcb8693c10a2a50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420fc4e1af7bee2a37bb944d3f355a71",
+    "url": "/mdpatankar/ibmdemo.github.io/index.html"
+  },
+  {
+    "revision": "27043fe201fc1035adc5",
+    "url": "/mdpatankar/ibmdemo.github.io/static/css/main.a9521173.chunk.css"
+  },
+  {
+    "revision": "7536539c0242743d4591",
+    "url": "/mdpatankar/ibmdemo.github.io/static/js/2.d04a2d35.chunk.js"
+  },
+  {
+    "revision": "be22591b514571347c955414644a3835",
+    "url": "/mdpatankar/ibmdemo.github.io/static/js/2.d04a2d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27043fe201fc1035adc5",
+    "url": "/mdpatankar/ibmdemo.github.io/static/js/main.1000d147.chunk.js"
+  },
+  {
+    "revision": "c982a89dcdb07a6c70ad",
+    "url": "/mdpatankar/ibmdemo.github.io/static/js/runtime-main.12b3dfc7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mdpatankar/ibmdemo.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
